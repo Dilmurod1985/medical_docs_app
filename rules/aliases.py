17898c1364d3d48e42dbb5e03d8849552c529a04
Dilmurod1985 <87082624+Dilmurod1985@users.noreply.github.com>
2026-01-24 14:10:08 +0500

    Delete rules directory

--- rules/aliases.py
+++
@@ -1,5 +0,0 @@
-FIELD_ALIASES = {
-    "number": ["№", "raqami", "номер", "id", "raqa", "raqam"],
-    "date": ["sana", "дата", "дата осмотра", "осмотр", "berilgan", "ko'rik"],
-    "series": ["seriyasi", "серия", "seriya", "mt", "tk", "ab"]
-}
