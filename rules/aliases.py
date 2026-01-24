50ec2898ae8e93d76afe12a02af5745659cccb3d
Dilmurod1985 <87082624+Dilmurod1985@users.noreply.github.com>
2026-01-24 13:50:27 +0500

    Create aliases.py

---
+++ rules/aliases.py
@@ -0,0 +1,5 @@
+FIELD_ALIASES = {
+    "number": ["№", "raqami", "номер", "id", "raqa", "raqam"],
+    "date": ["sana", "дата", "дата осмотра", "осмотр", "berilgan", "ko'rik"],
+    "series": ["seriyasi", "серия", "seriya", "mt", "tk", "ab"]
+}
